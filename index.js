e3401ab9bcf802fc4d936423e5201844d7828b56
Joran Quinten <joran.quinten@greenhousegroup.com>
2018-12-08 00:50:22 +0100

    Working POST api response

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST");
+});
 const port = 8080;
 
 const contentful = require("contentful");
 const client = contentful.createClient({
   // This is the space ID. A space is like a project folder in Contentful terms
-  space: process.env.CTF_SPACE_ID,
+  space: process.env.ctf_space_id,
   // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
-  accessToken: process.env.CTF_CDA_TOKEN
+  accessToken: process.env.ctf_cda_token
 });
 
 const fulfillmentTemplate = require("./utils/fulfillment-template");
@@ -28,11 +33,7 @@ async function getSuggestion() {
   }
 }
 
-app.get("/api/suggestion", (req, res) => {
-  //res.send("Hello World!!!");
-  console.log(fulfillmentTemplate.getTemplate("title", "description"));
-
-  // getting all suggestions
+app.post("/", (req, res) => {
   getSuggestion().then(suggestions => {
     // get a random entry
     const count = suggestions.total;
@@ -42,7 +43,6 @@ app.get("/api/suggestion", (req, res) => {
 
     res.json(fulfillmentTemplate.getTemplate("Ik heb een idee", title));
   });
-  //res.json(getSuggestion());
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen();
